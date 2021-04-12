129180b9640bdb02add1b782d6871ce1c3f842b9
pivotrooky <towagner@gmail.com>
2021-04-12 20:18:31 -0300

    Improves credit.c

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -2,67 +2,103 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int digit(int n);
+int get_digit_or_sum(int n);
+int get_specific_digit(long input, int index);
+int process_digit(int num, int index);
+void validate(int sum, int digits[]);
+
 int main(void)
 {
-    
+
     long input;
+    int digits[16];
+    int processed_digits[16];
 
     input = get_long("What's your credit card number?\n"); //get card number
-     
-    int d2 = (input % 100) / 10; //get every other digit
-    int d4 = (input % 10000) / 1000;
-    int d6 = (input % 1000000) / 100000;
-    int d8 = (input % 100000000) / 10000000;
-    int d10 = (input % 10000000000) / 1000000000;
-    int d12 = (input % 1000000000000) / 100000000000;
-    int d14 = (input % 100000000000000) / 10000000000000;
-    int d16 = (input % 10000000000000000) / 1000000000000000;
 
-    int s2 = digit(2 * d2); //get products and the sum of their digits
-    int s4 = digit(2 * d4);
-    int s6 = digit(2 * d6);
-    int s8 = digit(2 * d8);
-    int s10 = digit(2 * d10);
-    int s12 = digit(2 * d12);
-    int s14 = digit(2 * d14);
-    int s16 = digit(2 * d16);
+    //loop through input and get all digits
+    for (int i = 0; i < 16; i++)
+    {
+        digits[i] = get_specific_digit(input, i);
+    }
 
-    int sum_a = s2 + s4 + s6 + s8 + s10 + s12 + s14 + s16;
+    //loop through and process digits
+    for (int i = 0; i < 16; i++)
+    {
+        processed_digits[i] = process_digit(digits[i], i);
+    }
 
-    int d1 = input % 10; //get remaining digits
-    int d3 = (input % 1000) / 100;
-    int d5 = (input % 100000) / 10000;
-    int d7 = (input % 10000000) / 1000000;
-    int d9 = (input % 1000000000) / 100000000;
-    int d11 = (input % 100000000000) / 10000000000;
-    int d13 = (input % 10000000000000) / 1000000000000;
-    int d15 = (input % 1000000000000000) / 100000000000000;
-    
-    int sum_b = d1 + d3 + d5 + d7 + d9 + d11 + d13 + d15;
+    int sum = processed_digits[0];
 
-    int sum = sum_a + sum_b;
+    //get sum of all processed digits
+    for (int i = 1; i < 16; i++)
+    {
+        sum += processed_digits[i];
+    }
 
+    validate(sum, digits);
+}
+
+int get_digit_or_sum(int n) //to distinguish between single digit and double digit products
+{
+    if (n < 10)
+    {
+        return n;
+    }
+
+    return (n % 10) + ((n % 100) / 10);
+}
+
+int get_specific_digit(long input, int index)
+{
+    return fmod(input, pow(10, index + 1)) / pow(10, index);
+}
+
+int process_digit(int num, int index)
+
+{
+    //first case: index is even. then, return number as is.
+    if (index % 2 == 0)
+    {
+        return num;
+    }
+
+    int num_doubled = num * 2;
+
+    //second case: index is odd and doubling the number results in a single digit. then, double it and return that single digit.
+    if (num_doubled < 5)
+    {
+        return num_doubled;
+    }
+
+    //third case: index is odd and doubling the number results in two digits. then, double it, add those digits and return the result.
+    return (num_doubled % 10) + ((num_doubled % 100) / 10);
+}
+
+
+
+void validate(int sum, int digits[])
+{
     if ((sum % 10) == 0) //check sum, then determine brand
     {
-        if ((d16 == 5) && ((d15 == 1) || ((d15 == 2) || (d15 == 3) || (d15 == 4) || (d15 == 5)))) 
+        if ((digits[15] == 5) && ((digits[14] == 1) || ((digits[14] == 2) || (digits[14] == 3) || (digits[14] == 4) || (digits[14] == 5))))
             // checks digits from 16 since it doesn't know where to start, is it Mastercard?
         {
             printf("MASTERCARD\n");
         }
 
-        else if ((d16 == 0) && ((d15 == 3) && ((d14 == 4) || (d14 == 7)))) //is it AMEX?
+        else if ((digits[15] == 0) && ((digits[14] == 3) && ((digits[13] == 4) || (digits[13] == 7)))) //is it AMEX?
         {
             printf("AMEX\n");
         }
 
-        else if ((d16 == 4) || (d16 + d15 + d14 == 0) & (d13 == 4)) //is it VISA?
+        else if ((digits[15] == 4) || (digits[15] + digits[14] + digits[13] == 0) & (digits[12] == 4)) //is it VISA?
         {
             printf("VISA\n");
         }
         else //in this case, the last digit of the sum being 0 may be random
         {
-            printf("INVALID\n"); 
+            printf("INVALID\n");
         }
     }
 
@@ -72,19 +108,3 @@ int main(void)
     }
 
 }
-
-int digit(int n) //to distinguish between single digit and double digit products
-{   
-    int r;
-    if (n >= 10)
-    {
-        r = (n % 10) + ((n % 100) / 10);  
-    }  
-    else
-    {
-        r = n;
-    }
-        
-    return r;
-}
-
