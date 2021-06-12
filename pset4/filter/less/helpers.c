803453f6c0040b956bbb2686717db9a4a97f794a
pivotrooky <towagner@gmail.com>
2021-06-12 16:45:40 -0300

    Adds grayscale, sepia, reflect from filter

--- pset4/filter/less/helpers.c
+++ pset4/filter/less/helpers.c
@@ -1,20 +1,64 @@
 #include "helpers.h"
+#include <math.h>
+
+int cap(int number);
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float total = image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue;
+            float average = round(total / 3);
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
+        }
+    }
     return;
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
+            float sepiaGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
+            float sepiaBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
+
+            image[i][j].rgbtRed = cap(round(sepiaRed));
+            image[i][j].rgbtGreen = cap(round(sepiaGreen));
+            image[i][j].rgbtBlue = cap(round(sepiaBlue));
+        }
+    }
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+
+        {   int tempRed = image[i][j].rgbtRed;
+            int tempGreen = image[i][j].rgbtGreen;
+            int tempBlue = image[i][j].rgbtBlue;
+
+            image[i][j].rgbtRed = image[i][width - j - 1].rgbtRed;
+            image[i][j].rgbtGreen = image[i][width - j - 1].rgbtGreen;
+            image[i][j].rgbtBlue = image[i][width - j - 1].rgbtBlue;
+
+            image[i][width - j - 1].rgbtRed = tempRed;
+            image[i][width - j - 1].rgbtGreen = tempGreen;
+            image[i][width - j - 1].rgbtBlue = tempBlue;
+        }
+    }
     return;
 }
 
@@ -22,4 +66,13 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     return;
+}
+
+int cap(int number)
+{
+    if (number > 255)
+    {
+        return 255;
+    }
+    return number;
 }
